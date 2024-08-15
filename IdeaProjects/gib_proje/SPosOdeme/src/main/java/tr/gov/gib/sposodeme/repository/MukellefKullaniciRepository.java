3d96ecb6d787027e217c0b818946eac6e8b62f74
Rumeysa İncebay <rumysainc78@gmail.com>
2024-08-15 09:10:20 +0000

    Revert "Merge branch 'master' into 'main'"
    This reverts merge request !1

--- IdeaProjects/gib_proje/SPosOdeme/src/main/java/tr/gov/gib/sposodeme/repository/MukellefKullaniciRepository.java
+++
@@ -1,7 +0,0 @@
-package tr.gov.gib.sposodeme.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tr.gov.gib.sposodeme.entity.MukellefKullanici;
-
-public interface MukellefKullaniciRepository extends JpaRepository<MukellefKullanici, Integer> {
-}
--- IdeaProjects/gib_proje/SPosOdeme/src/main/java/tr/gov/gib/sposodeme/repository/OdemeRepository.java
+++
@@ -1,7 +0,0 @@
-package tr.gov.gib.sposodeme.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tr.gov.gib.sposodeme.entity.Odeme;
-
-public interface OdemeRepository extends JpaRepository<Odeme, Integer> {
-}
