f79fff0e38ae9b916b04bb8662504aca9f788023
Rumeysa İncebay <rumysainc78@gmail.com>
2024-08-15 10:15:35 +0300

    Spos servis eklendi

---
+++ IdeaProjects/gib_proje/SPosOdeme/src/main/java/tr/gov/gib/sposodeme/repository/MukellefKullaniciRepository.java
@@ -0,0 +1,7 @@
+package tr.gov.gib.sposodeme.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tr.gov.gib.sposodeme.entity.MukellefKullanici;
+
+public interface MukellefKullaniciRepository extends JpaRepository<MukellefKullanici, Integer> {
+}
---
+++ IdeaProjects/gib_proje/SPosOdeme/src/main/java/tr/gov/gib/sposodeme/repository/OdemeDetayRepository.java
@@ -0,0 +1,7 @@
+package tr.gov.gib.sposodeme.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tr.gov.gib.sposodeme.entity.OdemeDetay;
+
+public interface OdemeDetayRepository extends JpaRepository<OdemeDetay, Integer> {
+}
