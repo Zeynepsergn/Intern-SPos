8b379ca35f29d334bac1d4edeea45cfd77e5a2a2
Rumeysa İncebay <rumysainc78@gmail.com>
2024-08-15 12:18:52 +0300

    Spos gelistirme ve ilk kod atımı @rumeysaInc

---
+++ src/test/java/tr/gov/gib/sposservice/SPosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package tr.gov.gib.sposservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SPosServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
